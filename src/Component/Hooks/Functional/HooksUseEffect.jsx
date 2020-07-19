1ee1c417384c23a0cbd013b47ed7918e83e6e70b
Hengky Wijaya <hannw171@outlook.com>
2020-07-19 13:11:48 +0700

    Kuliah #35
    Penerpaan  hooks useEffect

--- src/Component/Hooks/Functional/HooksUseEffect.jsx
+++ src/Component/Hooks/Functional/HooksUseEffect.jsx
@@ -1,20 +1,48 @@
-import React, {useEffect,useState} from 'react'
-import {Container} from 'reactstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Button, Alert, Table } from 'reactstrap'
+import axios from 'axios'
+
+const api = 'http://localhost:3001'
 
 export default function HooksUseEffect() {
-    
-    const[namaLengkap, setNamaLengkap] = useState({nama:'Han', marga:'Wijaya'})
 
-    useEffect(()=>{
+    const [mahasiswa, setMahasiswa] = useState([])
+
+    useEffect(() => {
         console.log("Memanggil Use Effect")
-        setNamaLengkap({nama:'Hengky',marga:'han'})
-    },[])
-    
+        axios.get(api + '/tampil').then(res => {
+            setMahasiswa(res.data.values)
+        })
+    }, [])
+
+
     return (
         <Container>
-            <h3>Profile User</h3>
-            <h4>Nama  : {namaLengkap.nama}</h4>
-            <h4>Marga : {namaLengkap.marga}</h4>
+            <h2>Data Mahasiswa</h2>
+            <navlink><Button color="success" href="/mahasiswa/tambah">Tambah Data</Button></navlink>
+            <hr />
+            <Table className="table-bordered">
+                <thead>
+                    <tr>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Jurusan</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mahasiswa.map(mahasiswa =>
+                        <tr key={mahasiswa.id_mahasiswa}>
+                            <td>{mahasiswa.nim}</td>
+                            <td>{mahasiswa.nama}</td>
+                            <td>{mahasiswa.jurusan}</td>
+                            <td>
+                                <Button onClick={() => this.delMahasiswa(mahasiswa.id_mahasiswa)} color="danger">Hapus</Button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
         </Container>
     )
 }
