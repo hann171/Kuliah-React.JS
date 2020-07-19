e8dd657a927cb122b96b2cb30c2b4c79cc394b29
Hengky Wijaya <hannw171@outlook.com>
2020-07-19 14:12:22 +0700

    Kuliah #36
    Pengenalan useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Parent from './Component/Class/Parent'
 import BootstrapComp from './Component/Class/BootstrapComp';
-import {BrowserRouter, Route, Switc} from 'react-router-dom'
+import { BrowserRouter, Route, Switc } from 'react-router-dom'
 import NavbarComp from './Component/Functional/NavbarComp';
 import HomePage from './Component/Functional/HomePage';
 import About from './Component/Functional/AboutComp';
@@ -12,6 +12,7 @@ import EditComp from './Component/Functional/EditComp';
 import KelasComp from './Component/Hooks/Class/KelasComp';
 import HooksComp from './Component/Hooks/Functional/HooksComp';
 import HooksUseEffect from './Component/Hooks/Functional/HooksUseEffect';
+import { CartContext } from './CartContext';
 //import logo from './logo.svg';
 //import './App.css';
 //import Home from './Component/Functional/Home';
@@ -21,21 +22,24 @@ import HooksUseEffect from './Component/Hooks/Functional/HooksUseEffect';
 const App = () => {
   return (
     <BrowserRouter>
-      <NavbarComp/>
-      <switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/about" component={About}/>
-        <Route exact path="/detail/:id" component={Detail}/>
-        <Route exact path="/mahasiswa" component={ListComp}/>
-        <Route exact path="/mahasiswa/tambah" component={TambahComp}/>
-        <Route exact path="/mahasiswa/edit" component={EditComp}/>
-        <Route exact path="/kelas" component={KelasComp}/>
-        <Route exact path="/hooks" component={HooksComp}/>
-        <Route exact path="/useeffects" component={HooksUseEffect}/>
-      </switch>
+      <CartContext.Provider value="ini adalah data context">
+        <NavbarComp />
+        <switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/detail/:id" component={Detail} />
+          <Route exact path="/mahasiswa" component={ListComp} />
+          <Route exact path="/mahasiswa/tambah" component={TambahComp} />
+          <Route exact path="/mahasiswa/edit" component={EditComp} />
+          <Route exact path="/kelas" component={KelasComp} />
+          <Route exact path="/hooks" component={HooksComp} />
+          <Route exact path="/useeffects" component={HooksUseEffect} />
+        </switch>
+      </CartContext.Provider>
     </BrowserRouter>
   );
 }
+
 
 //sebelumnya di div ada dashboard dan home
 export default App;
