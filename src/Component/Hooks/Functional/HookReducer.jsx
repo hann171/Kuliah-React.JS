c2b7e40291f8864f0efa440052a7834f89319d65
Hengky Wijaya <hannw171@outlook.com>
2020-07-21 14:33:27 +0700

    Kuliah #38
    useReducer dengan kompleks state

--- src/Component/Hooks/Functional/HookReducer.jsx
+++ src/Component/Hooks/Functional/HookReducer.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-import { Container, Row, Col, Card, CardImg } from 'reactstrap'
+import React, {useReducer} from 'react'
+import { Container, Row, Col, Card, CardImg, Button } from 'reactstrap'
+
+const initialState = {
+    jumlah: 0,
+    hargaSatuan: 100000,
+    hargaTotal: 0
+}
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case 'tambah': return {
+            ...state,
+            jumlah: state.jumlah + 1,
+            hargaTotal: state.hargaSatuan + (state.hargaSatuan*state.jumlah)
+        }
+        case 'kurang': return {
+            ...state,
+            jumlah: state.jumlah - 1,
+            hargaTotal: (state.hargaSatuan*state.jumlah) - state.hargaSatuan
+        }
+        default:
+            return state
+    }
+}
 
 export default function HookReducer() {
+
+    const[count, dispatch] = useReducer(reducer,initialState)
     return (
         <Container>
             <Row>
@@ -13,8 +38,15 @@ export default function HookReducer() {
                 <Col xs="6">
                     <h1>TES</h1>
                     <p>harga</p>
-                    <h3>Rp. 100.000</h3>
+                    <h3>Rp. {count.hargaSatuan}</h3>
                     <p>Jumlah</p>
+                    <Row>
+                        <Col><Button onClick={()=>dispatch({type:'tambah'})} color="danger">+</Button></Col>
+                        <Col><Button>{count.jumlah}</Button></Col>
+                        <Col><Button onClick={()=>dispatch({type:'kurang'})}>-</Button></Col>\
+
+                    </Row>
+                    <Button color="success">Total {count.hargaTotal}</Button>
                 </Col>
             </Row>
         </Container>
